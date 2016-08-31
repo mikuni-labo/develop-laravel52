0fedc5b48821b865fb58107e27de7d6c1e752626
Kuniyasu_Wada <elephunk.com@icloud.com>
2016-08-31 23:41:54 +0900

    gulp file追記。 各種設定ファイル追記。 gitignore追記。

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+require('es6-promise').polyfill();
 var elixir = require('laravel-elixir');
 
 /*
@@ -12,5 +13,33 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+	// LESS
+	//mix.less('app.less').version('css/app.css');
+	
+	// SASS
+	mix.sass('style.scss').version('css/style.css');
+	//mix.sass('admin.style.scss').version('css/admin.style.css');
+	
+	// JS
+//	mix.scripts([
+//		'jquery-2.1.4.min.js'
+//	],
+//		'jquery.js'
+//	)
+//	mix.scripts([
+//		'jquery.ui.core.min.js',
+//		'jquery.ui.datepicker-ja.min.js',
+//		'jquery.ui.datepicker.min.js',
+//		'jquery.ui.ympicker.js',
+//	],
+//		'public/js/datepicker.js'//datepicker
+//	)
+//	.scripts([
+//		'common.js',
+//		'change.input.color.js',
+//		'log.js',
+//	],
+//		'public/js/main.js'
+//	);
+	
 });
