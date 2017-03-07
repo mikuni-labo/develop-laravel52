6ed225e68e46c5416be91d0e9cbab6fdd497f69a
Kuniyasu Wada <wada@n-di.co.jp>
2017-03-07 09:40:08 +0900

    メディスンAPI開発用コントローラ

---
+++ app/Http/Controllers/MedistockApiController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Api Response...
+ * 
+ * @author Kuniyasu_Wada
+ */
+class MedistockApiController extends Controller
+{
+	/**
+	 * コンストラクタ
+	 */
+	public function __construct()
+	{
+		//
+	}
+	
+	/**
+	 * 受注取得
+	 */
+	public function getOrder()
+	{
+		dd('test');
+	}
+	
+}
