8145f7948257afa4947c8a2687b0f9fa3ce72efc
Kuniyasu_Wada <elephunk.com@icloud.com>
2017-04-15 13:54:54 +0900

    Notificationsはまた後ほど・・

--- app/Lib/Api/VideoCloud/NotificationsClient.php
+++ app/Lib/Api/VideoCloud/NotificationsClient.php
@@ -9,4 +9,21 @@ namespace App\Lib\Api\VideoCloud;
  */
 Trait NotificationsClient
 {
+    /**
+     * Get a list of all notification subscriptions for the account
+     * 
+     * @see     http://docs.brightcove.com/en/video-cloud/cms-api/references/cms-api/versions/v1/index.html#api-notificationGroup-Get_Subscriptions_List
+     * @example https://cms.api.brightcove.com/v1/accounts/:account_id/subscriptions
+     * @return  mixed
+     */
+    public function getSubscriptionsList()
+    {
+        $url = "{$this->getCmsUrl()}/v1/accounts/{$this->getAccountId()}/subscriptions";
+        $header = [
+            'Content-type: application/json',
+            "Authorization: Bearer {$this->getAccessToken()}",
+        ];
+        
+        return $this->call('GET', $url, $header);
+    }
 }
