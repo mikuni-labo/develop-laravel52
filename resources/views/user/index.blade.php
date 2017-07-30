ffdbae1fcbe8fb218516135b359916e935a70777
KuniyasuWada <elephunk.com@i.softbank.jp>
2017-07-31 01:23:36 +0900

    オプションもいろいろ検証

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -123,7 +123,8 @@
                     <h2 class="h2 page-header animated bounce"><span class="glyphicon glyphicon-user"></span>&nbsp;ユーザ一覧</h2>
                     
                     @if(count($results) > 0)
-                            <table id="user-index-table" class="table table-hover table-condensed table-striped">
+                        <table id="user-index-table" class="table table-hover table-condensed table-striped">
+                            @if(false)
                                 <colgroup>
                                     <col width="8%">
                                     <col width="21%">
@@ -133,42 +134,44 @@
                                     <col width="10%">
                                     <col width="10%">
                                 </colgroup>
-                                <thead>
-                                    <tr>
-                                        <th class="text-center">ID</td>
-                                        <th class="text-center">ユーザー名</td>
-                                        <th class="text-center">メールアドレス</td>
-                                        <th class="text-center">権限</td>
-                                        <th class="text-center">ステータス</td>
-                                        <th class="text-center">編集</td>
-                                        <th class="text-center">削除</td>
+                            @endif
+                            
+                            <thead>
+                                <tr>
+                                    <th class="text-center">ID</td>
+                                    <th class="text-center">ユーザー名</td>
+                                    <th class="text-center">メールアドレス</td>
+                                    <th class="text-center">権限</td>
+                                    <th class="text-center">ステータス</td>
+                                    <th class="text-center">編集</td>
+                                    <th class="text-center">削除</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($results as $result)
+                                    <tr class="<?php if($result->status == '0' || !empty($result->deleted_at)):?>gray_out<?php endif;?>">
+                                        <td class="text-center">{{ $result->id }}</td>
+                                        <td class="text-center">{{ $result->last_name }} {{ $result->first_name }}</a></td>
+                                        <td class="text-center">{{ $result->email }}</a></td>
+                                        <td class="text-center">{{ $Fixed['role'][$result->role] }}</a></td>
+                                        <td class="text-center">@if( $result->status == '1' ) <span class="text-success">有効</span> @else <span class="text-danger">無効</span> @endif</td>
+                                        
+                                        <td class="text-center"><a href="/user/edit/{{ $result->id }}" class="btn btn-sm btn-success">
+                                            <span class="glyphicon glyphicon-edit"></span>&nbsp;編集</a>
+                                        </td>
+                                        <td class="text-center">
+                                            @if($result->deleted_at)
+                                                <a href="/user/restore/{{ $result->id }}" class="btn btn-sm btn-info" data-toggle="confirmation" onclick="if(!confirm('復旧させますか?')) return false;">
+                                                <span class="glyphicon glyphicon-repeat"></span>&nbsp;復旧</a>
+                                            @else
+                                                <a href="/user/delete/{{ $result->id }}" class="btn btn-sm btn-danger" data-toggle="confirmation" onclick="if(!confirm('本当に削除しますか?')) return false;">
+                                                <span class="glyphicon glyphicon-trash"></span>&nbsp;削除</a>
+                                            @endif
+                                        </td>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($results as $result)
-                                        <tr class="<?php if($result->status == '0' || !empty($result->deleted_at)):?>gray_out<?php endif;?>">
-                                            <td class="text-center">{{ $result->id }}</td>
-                                            <td class="text-center">{{ $result->last_name }} {{ $result->first_name }}</a></td>
-                                            <td class="text-center">{{ $result->email }}</a></td>
-                                            <td class="text-center">{{ $Fixed['role'][$result->role] }}</a></td>
-                                            <td class="text-center">@if( $result->status == '1' ) <span class="text-success">有効</span> @else <span class="text-danger">無効</span> @endif</td>
-                                            
-                                            <td class="text-center"><a href="/user/edit/{{ $result->id }}" class="btn btn-sm btn-success">
-                                                <span class="glyphicon glyphicon-edit"></span>&nbsp;編集</a>
-                                            </td>
-                                            <td class="text-center">
-                                                @if($result->deleted_at)
-                                                    <a href="/user/restore/{{ $result->id }}" class="btn btn-sm btn-info" data-toggle="confirmation" onclick="if(!confirm('復旧させますか?')) return false;">
-                                                    <span class="glyphicon glyphicon-repeat"></span>&nbsp;復旧</a>
-                                                @else
-                                                    <a href="/user/delete/{{ $result->id }}" class="btn btn-sm btn-danger" data-toggle="confirmation" onclick="if(!confirm('本当に削除しますか?')) return false;">
-                                                    <span class="glyphicon glyphicon-trash"></span>&nbsp;削除</a>
-                                                @endif
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                @endforeach
+                            </tbody>
+                        </table>
                     @else
                         <p>条件に一致するデータがありません...</p>
                     @endif
@@ -207,7 +210,46 @@
                 } 
             }); 
             
-            $("#user-index-table").DataTable();
+            $("#user-index-table").DataTable({
+                // 件数切替機能 無効
+/*                 lengthChange: false, */
+
+                // 検索機能 無効
+/*                 searching: false, */
+
+                // ソート機能 無効
+/*                 ordering: false, */
+
+                // 情報表示 無効
+/*                 info: false, */
+
+                // ページング機能 無効
+/*                 paging: false, */
+
+                // 初期表示時優先並び替え順（[ [ 列番号, 昇順降順 ], ... ] の形式で、指定しない場合は空配列）
+/*                 order: [0, "asc"], */
+                
+                // 横スクロールバーを有効にする (scrollXはtrueかfalseで有効無効を切り替えます)
+/*                 scrollX: true */
+                
+                // 縦スクロールバーを有効にする (scrollYは200, "200px"など「最大の高さ」を指定します)
+/*                 scrollY: true */
+                
+                // 各列デフォルト値
+                columnDefs: [
+/*                     { targets: 0, visible: false },// 非表示 */
+/*                     { targets: 1, width: 150 }// 幅 */
+                ],
+                
+                // 件数切替の値を10～50の10刻みにする
+/*                 lengthMenu: [ 10, 20, 30, 40, 50 ], */
+                
+                // 件数のデフォルトの値を50にする
+/*                 displayLength: 50,  */
+
+                // 状態を保存する機能をつける
+                stateSave: true,
+            });
         });
     </script>
 @endsection
